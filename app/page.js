6a34198e3194687719c7d340a1a7164e9bbeb0e4
Garrett Gee <garrett.m.gee@gmail.com>
2024-02-18 21:17:09 -0500

    made calendar component show

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Calendar from "@/components/Calendar" 
+import Calendar from "@/components/calendar/calendar.js" 
 // import clientPromise from "@/app/lib/mongodb.js"; 
 export default function Home() {
   return (
