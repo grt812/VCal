67c88779c76df41fa28b95fdbd7f01905ad714af
Garrett Gee <garrett.m.gee@gmail.com>
2024-03-01 23:54:17 -0500

    made buttons look more aesthetically appealing

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -3,9 +3,9 @@
 export default function Navigation({currentDate, prevMonth, nextMonth}) {
     return(
         <div id="navigation" className="flex header">
-            <button onClick={prevMonth}>&lt;</button>
+            <button className="arrow-button" onClick={prevMonth}>&lt;</button>
             <span class="grow text-center">{currentDate.toLocaleString('default', { month: 'long', year: 'numeric' })}</span>
-            <button onClick={nextMonth}>&gt;</button>
+            <button className="arrow-button" onClick={nextMonth}>&gt;</button>
         </div>
     );
 
